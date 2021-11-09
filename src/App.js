f19ba14e5a5fb5c262669a8b099776189a004df9
ndjndj <teq.ndj@gmail.com>
2021-11-10 01:17:29 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Sample from './components/react-draggable/sample'
+import Sample from './components/mui/sample'
 
 function App() {
   return (
