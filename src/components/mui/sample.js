0de355897bc4d7e3b3dff219829e2a03bd45526e
ndjndj <teq.ndj@gmail.com>
2021-11-10 01:15:14 +0900

    button sample

---
+++ src/components/mui/sample.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+import Button from '@material-ui/core';
+
+export default function Sample() {
+
+    return (
+        <Button variant="contained" color="primary">
+            Test
+        </Button>
+    )
+  }
