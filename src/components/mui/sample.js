d84923ad5e44ed536a58758ce613b02cdef969d7
ndjndj <teq.ndj@gmail.com>
2021-11-11 00:20:39 +0900

    Typography

--- src/components/mui/sample.js
+++ src/components/mui/sample.js
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 export default function Sample() {
 
