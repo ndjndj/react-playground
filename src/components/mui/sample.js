d5b3ff63333c54635433247ed52e4e0780a9f249
ndjndj <teq.ndj@gmail.com>
2021-11-11 00:39:24 +0900

    import Drawer

--- src/components/mui/sample.js
+++ src/components/mui/sample.js
@@ -9,6 +9,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import Drawer from "@material-ui/core/Drawer";
 
 export default function Sample() {
 
