6bc2fb2f5be5374bd2791d803895405b54d1616b
ndjndj <teq.ndj@gmail.com>
2021-11-11 00:36:56 +0900

    appbar sample

--- src/components/mui/sample.js
+++ src/components/mui/sample.js
@@ -4,12 +4,21 @@ import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
 
 export default function Sample() {
 
     return (
         <React.Fragment>
             <AppBar>
+                <Toolbar>
+                    <IconButton>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography>
+                        Sample
+                    </Typography>
+                </Toolbar>
             </AppBar>
             <Button variant="contained" color="primary">
                 Test
