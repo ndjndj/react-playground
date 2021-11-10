15e9483b283d2a81e918392715ec4ddfd146cf45
ndjndj <teq.ndj@gmail.com>
2021-11-11 00:19:39 +0900

    appbutton

--- src/components/mui/sample.js
+++ src/components/mui/sample.js
@@ -1,11 +1,17 @@
-import { React } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
+import AppBar from "@material-ui/core/AppBar";
 
 export default function Sample() {
 
     return (
-        <Button variant="contained" color="primary">
-            Test
-        </Button>
+        <React.Fragment>
+            <AppBar>
+            </AppBar>
+            <Button variant="contained" color="primary">
+                Test
+            </Button>
+        </React.Fragment>
+
     )
   }
