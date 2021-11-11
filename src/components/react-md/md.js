5d2d9fd9e1e292f3ba23ec0e13b6fc72c2d27159
ndjndj <teq.ndj@gmail.com>
2021-11-12 00:03:32 +0900

    fix

--- src/components/react-md/md.js
+++ src/components/react-md/md.js
@@ -1,7 +1,8 @@
+//https://qiita.com/dobusarai/items/d6c4b98d8696bc382fa6
+
 import React, {useState} from 'react'
 import MarkdownIt from 'markdown-it'
 import MdEditor, {Plugins} from 'react-markdown-editor-lite'
 import 'react-markdown-editor-lite/lib/index.css';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/vs.css'; // highlight.jsの好みのスタイルをここで指定
-//https://qiita.com/dobusarai/items/d6c4b98d8696bc382fa6
