745b8fbaf7b0cee946e0d1c83ba1f1d5653a7abd
ndjndj <teq.ndj@gmail.com>
2021-11-12 00:03:19 +0900

    import

---
+++ src/components/react-md/md.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react'
+import MarkdownIt from 'markdown-it'
+import MdEditor, {Plugins} from 'react-markdown-editor-lite'
+import 'react-markdown-editor-lite/lib/index.css';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/vs.css'; // highlight.jsの好みのスタイルをここで指定
+//https://qiita.com/dobusarai/items/d6c4b98d8696bc382fa6
