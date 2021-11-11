41c9638e8287c9a34071df87bdafb061d1e59b54
ndjndj <teq.ndj@gmail.com>
2021-11-12 00:54:03 +0900

    mdeSample with mde.js

---
+++ src/components/react-md/mde.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactMde from "react-mde";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import ReactMarkdown from "react-markdown";
+
+export default function MdeSample() {
+  const [value, setValue] = React.useState("**Hello world!!!**");
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div style={{ width: "50%", marginRight: 10 }}>
+        <ReactMde value={value} onChange={setValue} />
+      </div>
+      <div style={{ width: "50%" }}>
+        Preview
+        <ReactMarkdown source={value} />
+      </div>
+    </div>
+  );
+}
