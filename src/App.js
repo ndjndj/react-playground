647b8fa003d79dd1212801700b50ca154f14d01b
ndjndj <teq.ndj@gmail.com>
2021-11-12 00:10:32 +0900

    md

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Sample from './components/mui/sample'
+import MdSample from './components/react-md/md';
 
 function App() {
   return (
     <div className="App">
-      <Sample />
+      <MdSample />
     </div>
   );
 }
