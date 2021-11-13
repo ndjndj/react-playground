a88a2950de4f36273d7b6964f0bd13121a51de04
ndjndj <teq.ndj@gmail.com>
2021-11-13 19:19:12 +0900

    ref

--- src/components/simplemde/simplemde.js
+++ src/components/simplemde/simplemde.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
-
+// https://qiita.com/t_okkan/items/0a3318f90ee6c4468f82
 const MarkDownEditor = () => {
 
     const [markdown, setMarkdown] = useState('');
